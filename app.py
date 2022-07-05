9310c896523d0588f32982fe3a2177b62137030e
Duc <s3952670@student.rmit.edu.au>
2022-07-05 22:36:24 +1000

    sucessfully get data from api

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
 import requests
-
+import pycountry
 api_key = 'edb33546896b6ad3e1d57b86d9c0c8db'
 
-country_code = input("Enter your country: ")
-city_name = input("Enter city: ")
+country_name = input("Enter your country: ").title()
+country_code = pycountry.countries.get(name=country_name).alpha_2
+city_name = input("Enter city: ").title()
 
-weather_data = requests.get( f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=metric&APPID={api_key}")
+# get data back in the form of json file
+weather_data = requests.get( f"https://api.openweathermap.org/data/2.5/weather?q={city_name + ',' + country_code}&units=metric&APPID={api_key}")
 
+# see raw data
 weather = weather_data.json()
-
 print(weather_data.json())
+
 # Status indicator 
 if weather_data.status_code == 200:
-    print('Weather bot is running')
+    print('----The weather bot is running----')
+elif weather_data.status_code == 200:
+    print('----City Not Found----')
 else:
-    print('Error: Cannot fetch URL')
+    print('----Error: Cannot fetch URL----')
 
-
+#temp
+print('Temperature: ', round(weather_data.json()['main']['temp']),'°')
+#clouds
+print(weather_data.json()['weather'][0]['main'] + ", " + weather_data.json()['weather'][0]['description'])
+#humidity
+print('Humidity:', weather_data.json()['main']['humidity'],'%')
+#windspeed 
+print('Wind Speed:', weather_data.json()['wind']['speed'], "m/s")
--- country.py
+++ country.py
@@ -1,7 +1,11 @@
 import pycountry
 
-pycountry.countries.search_fuzzy('United Kingdom')
+test = pycountry.countries.get(name='United Kingdom').alpha_2
+
+test1 = pycountry.countries.search_fuzzy('United Kingdom')
 
 
+
+print(test)
 # for x in range (len(pycountry.countries)):
 #     print (list(pycountry.countries)[x])
