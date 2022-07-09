42f24aeb06d80c11af8939b1fd84c4b96ab15ff3
Duc <s3952670@student.rmit.edu.au>
2022-07-09 15:17:24 +1000

    beta version 1.0

--- app.py
+++ app.py
@@ -1,31 +1,53 @@
 import requests
 import pycountry
-api_key = 'edb33546896b6ad3e1d57b86d9c0c8db'
+from postTweet import Tweet
 
+
+
+# get user input : country and city name  
 country_name = input("Enter your country: ").title()
-country_code = pycountry.countries.get(name=country_name).alpha_2
+if country_name != None or country_name != "":
+    country_code = pycountry.countries.get(name=country_name).alpha_2
 city_name = input("Enter city: ").title()
 
 # get data back in the form of json file
+api_key = 'edb33546896b6ad3e1d57b86d9c0c8db'
 weather_data = requests.get( f"https://api.openweathermap.org/data/2.5/weather?q={city_name + ',' + country_code}&units=metric&APPID={api_key}")
 
 # see raw data
 weather = weather_data.json()
-print(weather_data.json())
+# print(weather_data.json())
 
 # Status indicator 
 if weather_data.status_code == 200:
     print('----The weather bot is running----')
-elif weather_data.status_code == 200:
+    #temp
+    temperature = 'Temperature: ' + str(round(weather_data.json()['main']['temp'])) + '°'
+    print(temperature)
+    #clouds
+    clouds = weather_data.json()['weather'][0]['main'] + ", " + weather_data.json()['weather'][0]['description']
+    print(clouds)
+    #humidity
+    humidity = 'Humidity:' + str(weather_data.json()['main']['humidity']) + '%'
+    print(humidity)
+    #windspeed 
+    wind = 'Wind Speed:'+ str(weather_data.json()['wind']['speed']) + "m/s"
+    print(wind)
+    # calling post method if only status code is == 200
+    Tweet.postTweet(city_name, temperature, clouds,humidity,wind)
+    
+elif weather_data.status_code == 400:
     print('----City Not Found----')
 else:
     print('----Error: Cannot fetch URL----')
 
-#temp
-print('Temperature: ', round(weather_data.json()['main']['temp']),'°')
-#clouds
-print(weather_data.json()['weather'][0]['main'] + ", " + weather_data.json()['weather'][0]['description'])
-#humidity
-print('Humidity:', weather_data.json()['main']['humidity'],'%')
-#windspeed 
-print('Wind Speed:', weather_data.json()['wind']['speed'], "m/s")
+
+
+
+
+
+
+
+
+
+
---
+++ postTweet.py
@@ -0,0 +1,35 @@
+import tweepy
+
+class Tweet:
+    
+    def __init__(self ,city_name, temperature, clouds, humidity, wind) -> None:
+        self.city_name = city_name
+        self.temperature = temperature
+        self.clouds = clouds
+        self.humidity = humidity
+        self.wind = wind
+
+    def postTweet(city_name, temperature, clouds, humidity, wind):
+        #Twitter tokens
+        consumer_key = 'Sw0EPH1KUKT8hR6AcsRa6aO5c'
+        consumer_secret = 'Bd5KHMfSeZ8EWXI5AMC3rcFSkP9oMJTNIX1h8Rq1LZnuujfnck'
+        access_token = '1544648798089072641-UUBqCSAPnpgS5nd2WRT7iatmAGl9Y0'
+        access_token_secret = '8JE8MjCyOZoSjrS511wtQoV6gZMAqQc6irWz6FwHVGuRH'
+        bearer_token = 'AAAAAAAAAAAAAAAAAAAAAKQXegEAAAAAII9lyNxt04yfv46Jvtx%2B%2BdmK%2FRU%3DO2e9ku0c8m1clQ6NG9VBgL0c6Uk2TdJ8QypnaCFcjZJxr8QUo6'
+
+
+        #authentication 
+        auth = tweepy.OAuth1UserHandler(
+        consumer_key, consumer_secret, access_token, access_token_secret
+        )
+        api = tweepy.API(auth)
+
+        #client idk wtf this does but it works 
+        Client = tweepy.Client(
+        consumer_key=consumer_key, 
+        consumer_secret=consumer_secret,
+        access_token=access_token, 
+        access_token_secret=access_token_secret
+        )
+        weather_tweet = (city_name + 2*"\n" + temperature + "\n" + humidity + "\n" + clouds  + "\n" + wind)
+        new_weather_tweet = Client.create_tweet(text= weather_tweet)
